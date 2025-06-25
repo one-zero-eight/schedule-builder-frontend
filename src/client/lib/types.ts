6b5cb73e0f0a64b73d1a1451788c51cf221e64df
Andrew Novikov <113201151+N1qro@users.noreply.github.com>
2025-06-25 14:33:31 +0300

    Refactor conflict types and data structures for conflicts
    Replaces the Collisions type with a new Conflict/ConflictResponse structure, updates all related components and utilities to use the new types, and simplifies the handling of conflicts throughout the codebase. Adds support for an OUTLOOK conflict type, updates test data, and refactors filtering and display logic to work with the new 2D array structure for conflicts.

--- src/client/lib/types.ts
+++ src/client/lib/types.ts
@@ -2,24 +2,45 @@ export enum CollisionType {
   ROOM = "room",
   TEACHER = "teacher",
   CAPACITY = "capacity",
+  OUTLOOK = "outlook",
 }
 
-export type LessonSlot = {
+type ConflictingLesson = {
   lesson_name: string;
   weekday: string;
   start_time: string;
   end_time: string;
   room: string;
   teacher: string;
-  group_name: string | null;
   students_number: number;
-  excel_range: string;
 };
 
-export type LessonSlotWithCollision = LessonSlot & { collision_type: CollisionType }
+export type CapacityConflict = ConflictingLesson & {
+  collision_type: CollisionType.CAPACITY,
+  group_name: string | string[],
+  excel_range: string,
+  room_capacity: number,
+}
 
-export type LessonSlotWithCollisions = LessonSlot & {
-  collisions: LessonSlotWithCollision[];
-};
+export type TeacherConflict = ConflictingLesson & {
+  collision_type: CollisionType.TEACHER,
+  group_name: string | string[],
+  excel_range: string,
+}
 
-export type Collisions = (LessonSlotWithCollision | LessonSlotWithCollisions)[]
+export type RoomConflict = ConflictingLesson & {
+  collision_type: CollisionType.ROOM,
+  group_name: string | string[],
+  excel_range: string,
+}
+
+export type OutlookConflict = ConflictingLesson & {
+  collision_type: CollisionType.OUTLOOK
+}
+
+export type Conflict = CapacityConflict | TeacherConflict | RoomConflict | OutlookConflict
+
+export type ConflictResponse = Conflict[][]
+
+
+
