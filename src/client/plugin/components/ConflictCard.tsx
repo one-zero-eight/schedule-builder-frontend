b7077d361eaafbc25d0a230a67c9c0dcaa8ebee2
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-07-05 14:16:28 +0300

    Revert "feat : add ignore button (#25)"
    This reverts commit db8b88978307372b1e09b28dafa8625098a8396c.

--- src/client/plugin/components/ConflictCard.tsx
+++ src/client/plugin/components/ConflictCard.tsx
@@ -1,24 +1,17 @@
 import LessonCard from "./LessonCard"
+
 import { type Conflict } from "../../lib/types";
 
 interface ConflictCardProps {
-  lesson: Conflict;
-  onIgnore?: () => void;
-  mode?: 'ignore' | 'restore';
+  lesson: Conflict
 }
 
 export default function Card(props: ConflictCardProps) {
-
-
   return (
     <div className="p-0.5 bg-gradient-to-b from-[#8C35F6] to-[#5C20A6] rounded-lg">
       <div className="p-1 bg-gradient-to-b from-[#323232] to-[#282828] rounded-[calc(0.5rem-1px)] select-none">
         <div className="text-start text-sm">
-          <LessonCard 
-            lesson={props.lesson} 
-            onIgnore={props.onIgnore} 
-            mode={props.mode}
-          />
+          <LessonCard {...props.lesson} />
         </div>
       </div>
     </div>
