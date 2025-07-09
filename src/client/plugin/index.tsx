2613745cb9b44714fe76c26f813dbb7811df9f8a
Andrew <andrey777-novikov777@yandex.ru>
2025-07-10 00:09:28 +0300

    Refactored: Renamed index.jsx to index.tsx

--- src/client/plugin/index.tsx
+++ src/client/plugin/index.tsx
@@ -4,6 +4,10 @@ import ApiTokenProvider from './components/apiToken/Provider';
 import './styles.css';
 
 const container = document.getElementById('index');
+if (!container) {
+  throw new Error('Could not find element with id "index"');
+}
+
 const root = createRoot(container);
 root.render(
   <ApiTokenProvider>
